6bf9cccf0a496bda6c371da97b0b45b2d9e8f2a4
heera9331 <heera9331@gmail.com>
2024-04-08 23:54:17 +0530

    string.h added

---
+++ DevikaOS/src/cdevika/include/string.h
@@ -0,0 +1,17 @@
+
+
+#include "cdevika/types.h"
+
+/**
+ * @desc return length of string
+ * @return void
+*/
+
+void strLength(string);
+
+/**
+ * @desc - str comparison, return 1 if both strings are equal else 0
+ * 
+ * @return int
+*/
+void strEqual(string str1, string str2);
