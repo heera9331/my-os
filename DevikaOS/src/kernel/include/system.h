92a9981586a3f728ee76108deb1a2f81943a155b
heera9331 <heera9331@gmail.com>
2024-04-09 03:28:51 +0530

    code restructured and working

--- DevikaOS/src/kernel/include/system.h
+++ DevikaOS/src/kernel/include/system.h
@@ -7,7 +7,7 @@
 #ifndef DEVIKAOS_SYSTEM_H
 #define DEVIKAOS_SYSTEM_H
 
-#include "types.h"
+#include "../cdevika/include/types.h"
 
 uint8 inportb (uint16 _port);
 void outportb (uint16 _port, uint8 _data);
